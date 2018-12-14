df1f128b8433cbfbc92818d007eeccc8aab5af6d
Geunyoung Kim <kgysmart@gmail.com>
2018-12-14 10:43:04 -0800

    Add comments

---
+++ first/explain/comments/main.go
@@ -0,0 +1,14 @@
+// Package main makes this package on execution program
+package main
+
+import "fmt"
+
+/*
+main function
+Go executions this program using this function.
+There should be only one main file in a main package.
+Executable programs are also called as "Commands".
+*/
+func main() {
+	fmt.Println("Hello Gopher")
+}
